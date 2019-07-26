a50d24282dd42af02b7fa246fff52da7e139fe88
Marek Dwulit <marek.dwulit@agilebeat.com>
2019-07-25 21:34:19 -0400

    WIP: Add draw tiles for given polygon

--- scripts/TileAlgebra.js
+++ scripts/TileAlgebra.js
@@ -1,34 +1,4 @@
-console.log("WOW IT WORKS");
 
-var myGradesCalculate = (function () {
-
-    // Keep this variable private inside this closure scope
-    var myGrades = [93, 95, 88, 0, 55, 91];
-
-    // Expose these functions via an interface while hiding
-    // the implementation of the module within the function() block
-
-    return {
-        average: function() {
-            var total = myGrades.reduce(function(accumulator, item) {
-                return accumulator + item;
-            }, 0);
-
-            return'Your average grade is ' + total / myGrades.length + '.';
-        },
-
-        failing: function() {
-            var failingGrades = myGrades.filter(function(item) {
-                return item < 70;
-            });
-
-            return 'You failed ' + failingGrades.length + ' times.';
-        }
-    }
-})();
-
-console.log(myGradesCalculate.failing()); // 'You failed 2 times.'
-myGradesCalculate.average(); // 'Your average grade is 70.33333333333333.'
 
 
 var tileAlgebra = (function (layerGroup) {
@@ -54,16 +24,26 @@ var tileAlgebra = (function (layerGroup) {
     var _long2tile = function (lon,zoom) { return (Math.floor((lon+180)/360*Math.pow(2,zoom))); };
     var _lat2tile = function (lat,zoom)  { return (Math.floor((1-Math.log(Math.tan(lat*Math.PI/180) + 1/Math.cos(lat*Math.PI/180))/Math.PI)/2 *Math.pow(2,zoom))); };
 
+    var _booleanOverlap = function (polygon, rec) {
+        return;
+    }
+
     return {
-        bbox_coverage: function (northEast, southWest, z) {
-            var layerGroup = L.layerGroup([]);
-            var stop_x = _long2tile(northEast.lng, z);
-            var start_y = _lat2tile(northEast.lat, z);
-            var start_x = _long2tile(southWest.lng, z);
-            var stop_y = _lat2tile(southWest.lat, z);
-            for (x = start_x; x < stop_x; x++) {
-                for (y = start_y; y < stop_y; y++) {
-                    layerGroup.addLayer(_get_as_rectangle(x,y,z));
+        bbox_coverage: function (northEast, southWest, z, polygon_gj) {
+            let layerGroup = L.layerGroup([]);
+            let stop_x = _long2tile(northEast.lng, z);
+            let start_y = _lat2tile(northEast.lat, z);
+            let start_x = _long2tile(southWest.lng, z);
+            let stop_y = _lat2tile(southWest.lat, z);
+            console.log(polygon_gj)
+            let polygon_tf = turf.polygon(polygon_gj.geometry.coordinates);
+            for (let x = start_x; x < stop_x; x++) {
+                for (let y = start_y; y < stop_y; y++) {
+                    let rect = _get_as_rectangle(x,y,z);
+                    let rect_tf = turf.polygon(rect.toGeoJSON().geometry.coordinates);
+                    if (turf.booleanContains(polygon_tf, rect_tf)) {
+                        layerGroup.addLayer(_get_as_rectangle(x,y,z));
+                    };
                 };
             };
             return layerGroup;
